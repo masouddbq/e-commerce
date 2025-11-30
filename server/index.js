b230ecdbdaff386cf481131bb91695e9e18c0dae
masoud dabbaghi <masouddabbaghi0@gmail.com>
2025-11-30 22:46:02 +0330

    Fix dotenv path for PM2

--- server/index.js
+++ server/index.js
@@ -6,8 +6,13 @@ import morgan from 'morgan';
 import { otpRouter } from './routes/otp.js';
 import { paymentRouter } from './routes/payment.js';
 import process from 'node:process';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+dotenv.config({ path: join(__dirname, '.env') });
 
 const app = express();
 
